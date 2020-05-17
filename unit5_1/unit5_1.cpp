35c355cdf6117c4a11865cd4833eaf4344b7a432
HaiBo-Xiao <54774523+HaiBo-Xiao@users.noreply.github.com>
2020-05-17 23:50:21 +0800

    添加项目文件。

---
+++ unit5_1/unit5_1.cpp
@@ -0,0 +1,47 @@
+﻿// unit5_1.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+using namespace std;
+
+void fn1();
+int x = 1,y = 2;
+
+int main()
+{
+    cout << "Begin.." << endl;
+    cout << "x=" <<x<< endl;
+    cout << "y=" << y << endl;
+    cout << "Evaluate x and y" << endl;
+
+    int x = 10, y = 20;
+    cout << "x=" << x << endl;
+    cout << "y=" << y << endl;
+    cout << "Step to fn1()" << endl;
+    fn1();
+
+    cout << "Back in main" << endl;
+    cout << "x=" << x << endl;
+    cout << "y=" << y << endl;
+
+    return 0;
+
+
+}
+void fn1()
+{
+    int y = 200;
+    cout << "x=" << x << endl;
+    cout << "y=" << y << endl;
+}
+
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+// 入门使用技巧: 
+//   1. 使用解决方案资源管理器窗口添加/管理文件
+//   2. 使用团队资源管理器窗口连接到源代码管理
+//   3. 使用输出窗口查看生成输出和其他消息
+//   4. 使用错误列表窗口查看错误
+//   5. 转到“项目”>“添加新项”以创建新的代码文件，或转到“项目”>“添加现有项”以将现有代码文件添加到项目
+//   6. 将来，若要再次打开此项目，请转到“文件”>“打开”>“项目”并选择 .sln 文件
